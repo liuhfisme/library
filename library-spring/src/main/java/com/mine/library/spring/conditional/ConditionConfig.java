4a2907a3dc8d191aa2ead40cdc0d2f533e226ee4
feifeiliu <liuff2513@163.com>
2017-03-29 15:32:51 +0800

    conditional 自定配置bean示例

---
+++ library-spring/src/main/java/com/mine/library/spring/conditional/ConditionConfig.java
@@ -0,0 +1,24 @@
+package com.mine.library.spring.conditional;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by feifei.liu on 2017/3/29.
+ */
+@Configuration
+public class ConditionConfig {
+
+    @Bean
+    @Conditional(WindowsCondition.class)
+    public ListService windowsListService() {
+        return new WindowsListService();
+    }
+
+    @Bean
+    @Conditional(LinuxCondition.class)
+    public ListService linuxListService() {
+        return new LinuxListService();
+    }
+}
---
+++ library-spring/src/main/java/com/mine/library/spring/conditional/ListService.java
@@ -0,0 +1,8 @@
+package com.mine.library.spring.conditional;
+
+/**
+ * Created by feifei.liu on 2017/3/29.
+ */
+public interface ListService {
+    public String showListCmd();
+}
