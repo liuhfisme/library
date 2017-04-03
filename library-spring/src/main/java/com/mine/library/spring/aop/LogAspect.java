77c7fadff5ae060b053d44a74cb667cc61a7c13e
liuff2513@163.com <liuhaifei6174>
2017-04-03 12:48:00 +0800

    AOP切面

--- library-spring/src/main/java/com/mine/library/spring/aop/LogAspect.java
+++ library-spring/src/main/java/com/mine/library/spring/aop/LogAspect.java
@@ -13,12 +13,20 @@ import java.lang.reflect.Method;
 /**
  * Created by liuff on 2017/3/27.
  */
-@Aspect
-@Component
+@Aspect //声明一个切面
+@Component //注册成Spring容器管理的Bean
 public class LogAspect {
+
+    /**
+     * 通过@Pointcut声明切点
+     */
     @Pointcut("@annotation(com.mine.library.spring.aop.Action)")
     public void annotationPointCut(){}
 
+    /**
+     *通过@After注解声明一个建言，并使用@Pointcut定义的切点
+     * @param joinPoint
+     */
     @After("annotationPointCut()")
     public void after(JoinPoint joinPoint) {
         MethodSignature signature = (MethodSignature) joinPoint.getSignature();
@@ -27,6 +35,10 @@ public class LogAspect {
         System.out.println("注解式拦截，"+action.name());
     }
 
+    /**
+     * 通过@Before注解声明一个建言，使用拦截规则作为参数
+     * @param joinPoint
+     */
     @Before("execution(* com.mine.library.spring.aop.DemoMethodService.*(..))")
     public void before(JoinPoint joinPoint) {
         MethodSignature signature = (MethodSignature) joinPoint.getSignature();
