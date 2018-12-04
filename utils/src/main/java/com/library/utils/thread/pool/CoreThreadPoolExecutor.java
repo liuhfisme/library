b06483dbba489e58159e9d1a7cdb8d0c8d278409
feifei.liu <feifei.liu@pcstars.com>
2018-12-04 22:53:44 +0800

    参数命名优化

--- utils/src/main/java/com/library/utils/thread/pool/CoreThreadPoolExecutor.java
+++ utils/src/main/java/com/library/utils/thread/pool/CoreThreadPoolExecutor.java
@@ -14,12 +14,12 @@ public class CoreThreadPoolExecutor {
     public static void main(String[] args) {
         int corePoolSize = 5;//核心池大小，意思是当超过这个范围的时候，就需要将新的线程放到等待队列中了即workQueue;
         int maximumPoolSize = 10;//线程池最大线程数量，表明线程池能创建的最大线程数
-        int keepAlivertime = 3;//当活跃线程数大于核心线程数，空闲的多余线程最大存活时间。
+        int keepAliveTime = 3;//当活跃线程数大于核心线程数，空闲的多余线程最大存活时间。
         TimeUnit unit = TimeUnit.SECONDS;//存活时间单位
         LinkedBlockingDeque<Runnable> workQueue = new LinkedBlockingDeque<>();//存放任务的队列--阻塞队列
         //handler ：超出线程范围（maximumPoolSize）和队列容量的任务的处理程序
         ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
-          corePoolSize, maximumPoolSize, keepAlivertime, unit, workQueue
+          corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue
         );
     }
 }
