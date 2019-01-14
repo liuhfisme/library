d56e27d4715782880107d8938d55835ae53bf69c
刘飞飞 <liufeifei02@beyondsoft.com>
2019-01-14 19:08:22 +0800

    【技术实现】excel、pdf

--- utils/src/main/java/com/library/utils/design/SingletonModeLazier.java
+++ utils/src/main/java/com/library/utils/design/SingletonModeLazier.java
@@ -9,11 +9,17 @@ package com.library.utils.design;
 public class SingletonModeLazier {
     private static SingletonModeLazier singletonModeLazier1;
     private static SingletonModeLazier singletonModeLazier2;
-    //volatile 保证，当singletonModeLazier变量被初始化成实例时，多个线程可以正确处理singletonModeLazier变量
+    /**
+     * volatile 保证，当singletonModeLazier变量被初始化成实例时，多个线程可以正确处理singletonModeLazier变量
+     */
     private volatile static SingletonModeLazier singletonModeLazier3;
     private SingletonModeLazier() {}
 
-    //没有加入synchronized 关键字的版本是线程不安全的
+    /**
+     * 没有加入synchronized 关键字的版本是线程不安全的
+     *
+     * @return SingletonModeLazier
+     */
     public static SingletonModeLazier getInstance() {
         if (singletonModeLazier1 == null) {
             singletonModeLazier1 = new SingletonModeLazier();
@@ -21,7 +27,11 @@ public class SingletonModeLazier {
         return singletonModeLazier1;
     }
 
-    //加入synchronized 关键字保证线程安全
+    /**
+     * 加入synchronized 关键字保证线程安全
+     *
+     * @return SingletonModeLazier
+     */
     public static synchronized SingletonModeLazier getInstance2() {
         if (singletonModeLazier2 == null) {
             singletonModeLazier2 = new SingletonModeLazier();
@@ -29,7 +39,11 @@ public class SingletonModeLazier {
         return singletonModeLazier2;
     }
 
-    //双重检查加锁
+    /**
+     * 双重检查加锁
+     *
+     * @return SingletonModeLazier
+     */
     public static SingletonModeLazier getInstance3() {
         if (singletonModeLazier3 == null) {
             synchronized (SingletonModeLazier.class) {
